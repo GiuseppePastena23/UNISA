b098bf970e93e71f1f6438a230e08fe8f7d5d3a1
GiuseppePastena23 <80394925+GiuseppePastena23@users.noreply.github.com>
2023-10-05 12:21:58 +0200

    a

---
+++ II Anno/POO/Laboratorio/51023/Peppe/Shirt.java
@@ -0,0 +1,14 @@
+public class Shirt
+{
+	public static void main(String args[])
+	{
+		if (args.length < 1)
+		{
+			System.out.println("Nessun argomento");
+		}
+		else
+		{
+			System.out.println("Stringa digitata: " + args[0]);
+		}
+	}
+}
