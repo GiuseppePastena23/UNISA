332c32ad0a31a84036973ff1aebb583c0f81290f
GiuseppePastena23 <80394925+GiuseppePastena23@users.noreply.github.com>
2023-10-12 11:54:18 +0200

    update esercizio Area POO

--- II Anno/POO/Laboratorio/Peppe/12-10-2023/Area.java
+++ II Anno/POO/Laboratorio/Peppe/12-10-2023/Area.java
@@ -1,10 +1,39 @@
+import java.util.Scanner;
+
 public class Area
   {
     public static void main (String[] args)
-  {
-    Triangolo t1 = new Triangolo (10, 5);
-    System.out.println("L'Area e': " + t1.calcolo());
-  }
+    {
+      System.out.println("Scegli Figura:\n 1. Quadrato\n 2. Triangolo");
+
+      Scanner scan = new Scanner(System.in);
+
+      int scelta = scan.nextInt();
+
+      switch(scelta)
+      {
+        case 1:
+          System.out.println("Inserisci il lato del Quadrato:");
+          double lato = scan.nextDouble();
+
+          Quadrato q1 = new Quadrato(lato);
+          System.out.println("Area del Quadrato:");
+          System.out.println(q1.calcoloArea());
+          break;
+        case 2:
+          System.out.println("Inserisci l'altezza del Triangolo:");
+          double altezza = scan.nextDouble();
+          System.out.println("inserisci la base del Triangolo:");
+          double base = scan.nextDouble();
+
+          Triangolo t1 = new Triangolo(altezza, base);
+          System.out.println("Area del triangolo:");
+          System.out.println(t1.calcolo());
+          break;
+
+      }
+
+    }
   }
 
 class Triangolo
@@ -22,5 +51,21 @@ class Triangolo
   {
     return ((base * altezza) / 2);
   }
+}
+
+class Quadrato
+{
+  double lato;
+
+  public Quadrato(double lato)
+  {
+    this.lato = lato;
+  }
+
+  double calcoloArea()
+  {
+    return (lato * lato);
+  }
+
 }
 
