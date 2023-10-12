9179faf5f536bdf6583fc16cba1a6e323e83d4b7
Pasquale Muraca <pasqualemuraca2003@gmail.com>
2023-10-12 11:51:08 +0200

    Update Triangolo.java

--- II Anno/POO/Laboratorio/Pasquale/12102023/Triangolo.java
+++ II Anno/POO/Laboratorio/Pasquale/12102023/Triangolo.java
@@ -3,13 +3,27 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Inserisci base");
-        double base = scan.nextDouble();
-        System.out.println("Inserisci altezza");
-        double altezza = scan.nextDouble();
 
-        Triangolo myTriangle = new Triangolo(base, altezza);
-        System.out.println("L'area e': " + myTriangle.calcolaArea());
+        System.out.println("Vuoi creare un triangolo o un quadrato?");
+        System.out.println("q / t");
+        char car = scan.nextLine().charAt(0);
+        if (car == 'q') {
+            System.out.println("Inserisci il lato");
+            double lato = scan.nextDouble();
+            Quadrato quadrato = new Quadrato(lato);
+            System.out.println("L'area e': " + quadrato.calcolaArea());
+        }
+        else if (car == 't') {
+            System.out.println("Inserisci base");
+            double base = scan.nextDouble();
+            System.out.println("Inserisci altezza");
+            double altezza = scan.nextDouble();
+
+            Triangolo myTriangle = new Triangolo(base, altezza);
+            System.out.println("L'area e': " + myTriangle.calcolaArea());
+        }
+
+        scan.close();
     }
 }
 
@@ -28,10 +42,15 @@ class Triangolo {
     }
 }
 
-class Ciao {
-    static int cont;
+class Quadrato {
+    double lato;
 
-    public Ciao() {
-        
+    public Quadrato(double lato) {
+        System.out.println("Creo oggetto Quadrato");
+        this.lato = lato;
+    }
+
+    double calcolaArea() {
+        return lato * lato;
     }
 }
